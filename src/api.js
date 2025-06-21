6059958b9fb27ff5dbd873b579926b55539d9086
ramon chuinca <ramonchuinca@hotmail.com>
2025-06-21 03:50:38 -0400

    atualizado

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
 // src/services/api.js
 import axios from 'axios'
 
+console.log('API URL:', import.meta.env.VITE_API_URL)
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: import.meta.env.VITE_API_URL + '/api',
 })
 
 export default api
+
